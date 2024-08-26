cbb9f3c566e47a9703c01107fc1a963c72c38bae
Lucas dos Santos Camilo <lucasdossantosnet@gmail.com>
2024-08-26 18:08:40 -0300

    Pequeno ajuste nas settings.py

--- backend/mymovielist/settings.py
+++ backend/mymovielist/settings.py
@@ -4,13 +4,13 @@ from pathlib import Path
 
 
 INSTALLED_APPS = [
-    'mymovielist',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mymovielist',
 ]
 
 # backend/mymovielist/settings.py
