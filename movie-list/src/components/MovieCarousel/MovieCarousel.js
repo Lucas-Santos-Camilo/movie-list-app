eb4ec7841030d363a7da19e4bb7da37193b2a078
Lucas dos Santos Camilo <lucasdossantosnet@gmail.com>
2024-08-28 19:31:44 -0300

    Ajustes Front-end

--- movie-list/src/components/MovieCarousel/MovieCarousel.js
+++ movie-list/src/components/MovieCarousel/MovieCarousel.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 import './MovieCarousel.css';
 
 const MovieCarousel = ({ title, apiUrl }) => {
@@ -21,14 +22,35 @@ const MovieCarousel = ({ title, apiUrl }) => {
         <div className="carousel">
             <h2>{title}</h2>
             <div className="carousel__container">
-                {movies.map(movie => (
-                    <img
-                        key={movie.id}
-                        className="carousel__image"
-                        src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-                        alt={movie.title}
-                    />
-                ))}
+                {movies.map(movie => {
+                    // Arredonda a nota para o número inteiro mais próximo
+                    const roundedVote = Math.round(movie.vote_average * 10); // Nota arredondada como porcentagem
+
+                    return (
+                        <div key={movie.id} className="carousel__item">
+                            <Link to={`/movie/${movie.id}`}>
+                                <img
+                                    className="carousel__image"
+                                    src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+                                    alt={movie.title}
+                                />
+                            </Link>
+                            <div className="carousel__details">
+                                <Link to={`/movie/${movie.id}`} className="carousel__title">
+                                    {movie.title}
+                                </Link>
+                                <div className="carousel__rating-container">
+                                    <div
+                                        className="carousel__progress-circle"
+                                        style={{ '--percent': roundedVote }}
+                                    >
+                                        <span className="carousel__rating">{`${roundedVote}%`}</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    );
+                })}
             </div>
         </div>
     );
